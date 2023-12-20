7eee1de76996c3199222ffca6dab1bf820f20665
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-20 17:49:56 +0100

    add check to see if login

--- website/views.py
+++ website/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
 def home(request):
-    
-    return render(request, 'home.html', {})
+    # check to see if login
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,'Yuo Have been Logged In!')
+            return redirect('home')
+
+        else:
+            messages.success(request,'Plaese Try again!')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
 
 
 def login_user(request):
