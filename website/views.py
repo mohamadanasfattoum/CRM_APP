85bf63c6bcf3b0dd17f464680e29d8812ea006b3
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-20 15:17:33 +0100

    add login_user & logout_user in urls

--- website/views.py
+++ website/views.py
@@ -8,9 +8,9 @@ def home(request):
     return render(request, 'home.html', {})
 
 
-def login(request):
+def login_user(request):
     pass
 
 
-def logout(request):
+def logout_user(request):
     pass
