356d0f578fce9b901c5db3cd2aac397500e142e0
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-20 15:10:41 +0100

    add 2 def ,login, logout

--- website/views.py
+++ website/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def home(request):
     
     return render(request, 'home.html', {})
+
+
+def login(request):
+    pass
+
+
+def logout(request):
+    pass
