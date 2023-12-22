b0d12ca15964a9bf4b2970231e60acad11bc3adb
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-22 21:25:58 +0100

    created new function def Register

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     # path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
 ]
+
--- website/views.py
+++ website/views.py
@@ -26,3 +26,7 @@ def logout_user(request):
     logout(request)
     messages.success(request,'You are logged out!')
     return redirect('home')
+
+
+def register_user(request):
+    return render(request, 'register.html', {})
