2a326ef4a87fe37b672b7db7075794fc509be343
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-23 22:00:24 +0100

    add registration html cod

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import SignUpForm
 
 def home(request): # ruf die seite # wenn ich zu page gehe : GET., wenn ich in page login mache : POST wie senden data. 
     # check to see if login
@@ -29,4 +29,17 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method =='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate, Login
+            user_name = form.cleaned_data['username'] 
+            password = form.cleaned_data['password1'] 
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You Have Success u y Registered! We come!')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
