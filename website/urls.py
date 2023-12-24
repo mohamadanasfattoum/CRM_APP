4d47d4ba45801c93d64c9527ea617f4718b14a72
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-24 12:44:05 +0100

    add record in urls

--- website/urls.py
+++ website/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
+    path('record/<int:pk>', views.customer_record, name='record'),
 ]
 
