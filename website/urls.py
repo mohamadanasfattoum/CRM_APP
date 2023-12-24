043be8a360e46c62afbb2f800af6e94841fc4328
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-24 14:13:53 +0100

    add delete_record in urls

--- website/urls.py
+++ website/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
     path('record/<int:pk>', views.customer_record, name='record'),
+    path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
 ]
 
