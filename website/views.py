2fb308c2ce1fa1b46177e618f07a870857d69d2d
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-24 14:08:41 +0100

    crate record.html and add record card

--- website/views.py
+++ website/views.py
@@ -48,3 +48,15 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html', {'form':form})
     return render(request, 'register.html', {'form':form})
+
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # look up records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, 'You Must Be Logged In To View That Page..')
+        return redirect('home')
+
