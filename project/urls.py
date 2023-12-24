d90d3ca9b6d366eae77ad51ed9573a58af99d7c6
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-24 19:19:07 +0100

    add api to urls

--- project/urls.py
+++ project/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
 
--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from .api import RecordListApi , RecordDetailApi
+
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -10,5 +12,8 @@ urlpatterns = [
     path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
     path('add_record/', views.add_record, name='add_record'),
     path('update_record/<int:pk>', views.update_record, name='update_record'),
+
+    path('api/list/', RecordListApi.as_view()),
+    path('api/list/<int:pk>', RecordDetailApi.as_view()), 
 ]
 
