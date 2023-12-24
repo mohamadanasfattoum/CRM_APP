6029f57d8229367990691e6c2edbce2de2070bc8
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-24 15:42:54 +0100

    add update_record to urls

--- website/urls.py
+++ website/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('record/<int:pk>', views.customer_record, name='record'),
     path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
     path('add_record/', views.add_record, name='add_record'),
+    path('update_record/<int:pk>', views.update_record, name='update_record'),
 ]
 
