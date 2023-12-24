8bfc1a993bed4b365d083eeaf394572a8bb9ebfd
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-24 14:25:26 +0100

    add delete_record btn in records

--- website/views.py
+++ website/views.py
@@ -60,3 +60,14 @@ def customer_record(request, pk):
         messages.success(request, 'You Must Be Logged In To View That Page..')
         return redirect('home')
 
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, '"Record Deleted Successfully...')
+        return redirect('home')
+    else:
+        messages.success(request, 'You Must Be Logged In To Delete..')
+        return redirect('home')
